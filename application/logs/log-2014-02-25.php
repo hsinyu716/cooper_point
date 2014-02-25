d4fb598888e8199a2450f960cd06347c074db7d4
hsinyu <z2493225@gmail.com>
2014-02-25 10:25:43 +0800

    del

---
+++ application/logs/log-2014-02-25.php
@@ -0,0 +1,6 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2014-02-25 03:14:49 --> 404 Page Not Found --> %7B%7Bpr.img%7D%7D
+ERROR - 2014-02-25 03:14:50 --> 404 Page Not Found --> %7B%7Bpr.img%7D%7D
+ERROR - 2014-02-25 03:15:40 --> 404 Page Not Found --> %7B%7Bpr.img%7D%7D
+ERROR - 2014-02-25 03:15:40 --> 404 Page Not Found --> %7B%7Bpr.img%7D%7D
--- application/views/index_view.php
+++ application/views/index_view.php
@@ -1,39 +1,5 @@
 <script>
 </script>
 
-<div class="index_allbox" style="margin:0 auto;">
-  <div class="index_infobtn"><a href="javascript:_show($('.info_box'));"><img src="images/info_btn.png" width="127" height="49" /></a></div>
-  <div class="index_startbtn"><a href="javascript:check_FB();"><img src="images/index_btn1.png" width="127" height="49" /></a></div>
-</div>
+<a href="javascript:check_FB();"><button>開始</button></a>
 
-<div class="info_box" style="display:none;">
-  <div class="info_title"><img src="images/info_title.png" width="134" height="30" /></div>
-  <div class="info_content">
-    <span class="info_red">壹、活動日期：</span>即日起至2014.2.28日止
-</p>
-    <p><span class="info_red">貳、活動方式：</span>於活動期間參加「A-Team召集令   勇闖艾雷泥煤禁地」網路活動，完成以下三步驟，就能參加抽獎，有機會獲得Ardbeg限量精美贈品喔！ <br />
-      1.  打擊泥煤怪物、成功通過三關考驗 <br />
-      2.  成功留下正確之個人資料者 <br />
-      3.  將活動分享至Facebook塗鴉牆(分享於塗鴉牆上之活動分享圖文，將作為個人戰績之憑據，請勿隨意刪除)</p>
-    <p><span class="info_red">參、獎項內容： </span><br />
-      Ardbeg 圍巾  10名 <br />
-      Ardbeg 真皮名片夾 10名 </p>
-    <p><span class="info_red">肆、中獎公布： </span><br />
-      中獎名單將於2014年3月10日公布Ardbeg官方粉絲團上。得獎者不另行通知，得獎名單自公告當日起，7天內未回覆資料者，視同放棄得獎資格。每人限一次得獎機會，同一地址不得重複中獎。獎項將於2014年3月31日前寄出。 </p>
-    <p class="info_red">伍、注意事項： </p>
-    <ol>
-      <ol>
-        <li>活動詳情及中獎名單依Ardbeg官方粉絲團上之公佈為準。 </li>
-        <li>寄送獎項之地址以中獎人所填之地址為準，主辦單位不負贈品無法送達之責任。 </li>
-        <li>本活動獎項寄送地區僅限台、澎、金、馬，主辦單位不處理郵寄獎品至海外地區之事宜。如贈品因無人收件或地址錯誤等原因遭退件，活動小組得通知中獎者辦理親領手續，中獎人逾2013年3月31日，如未親自領取則視同放棄中獎。 </li>
-        <li>贈品不得轉售或是折換現金，規格外觀以實物為準，主辦單位保留更換等值或更高價值贈品的權利。 </li>
-        <li>參與者應詳細填寫個人基本資料及聯絡方式，若因參與者聯絡資訊填寫不正確，導致無法聯繫，將視為自動棄權。 </li>
-        <li>寄送贈品過程中，若發生任何毀壞、錯遞、延遲或遺失，然非主辦單位疏失造成者，主辦單位恕不負責，且獎品經簽收受領後，如有遺失、盜領或自行拋棄、損毀，主辦單位不負責補發獎品。所有獎項日後之使用與維修，主辦單位概不負責。 </li>
-        <li>若得獎人因資格不符、願意放棄中獎資格者或超過限定時間未提供得獎資料，視同失去得獎資格，不另行網路公告。 </li>
-        <li>得獎者同意配合主辦單位將得獎者名單公佈於活動網站與Ardbeg官方粉絲團上，以作為活動宣傳。 </li>
-        <li>依中華民國稅法規定，獎項金額若超過新台幣1,000元，得獎人需做得獎申報扣繳，得獎人須依規定填寫個人資料並繳交相關中獎收據、身分證件影本，得獎者若無法配合，則視為自動棄權，不具得獎資格。獎項價值超過新台幣20,000元者，中獎者依法需自付10%稅額〈海外人士或不具中華民國身分者，機會中獎所得稅為20%〉，主辦單位年底將開立各類所得稅扣繳憑單給得獎者。如果得獎者不願意給付得獎商品之稅額，則視為得獎者自動棄權，不具得獎資格。 </li>
-      </ol>
-    </ol>
-  本活動如因故無法舉行，主辦單位保有修改活動辦法、獎項或終止本活動之權利，如有變動將另行公告於Ardbeg官方粉絲團上。</div>
-<div class="result_startbtn"><a href="javascript:_show($('.info_box'));"><img src="images/info_btn2.png" width="149" height="48" /></a></div>
-</div>
